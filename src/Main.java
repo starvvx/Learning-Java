0c43a32212c06bd666709e604f63e0c48f1abfaa
starvvx <vaibhavvstar31@gmail.com>
2021-06-14 17:21:52 +0530

    Collection: ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-//        studentMarks [] marksInMidsem = {
-//                new studentMarks("Pushkar",90,80,70),
-//                new studentMarks("Jaljala",80,90,40),
-//                new studentMarks("muchhad",70,90,60),
-//                new studentMarks("Anupam",90,90,90)
-//        };
+        Collection<Integer> v= new ArrayList<Integer>();
+        v.add(10);
+        v.add(10);
+        v.add(13);
 
-        studentMarks marks = new studentMarks("Pushkar",70,40,90);
-
-        for(grades grade:marks.getSortedByGrades()) {
-                System.out.println(grade.getGrade().getTitle());
+        Iterator<Integer> itr = v.iterator();
+        while (itr.hasNext()) {
+            int i = itr.next();
+            if(i > 10) {
+                itr.remove();
+            }
         }
+        System.out.println(v);
     }
 
 }
--- src/getAvgMarks.java
+++
@@ -1,3 +0,0 @@
-public interface getAvgMarks {
-    double calculateAvgMarks();
-}
