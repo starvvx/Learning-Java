e16688ebfc05eef38dabfe9c969ebc9ff9eacd0d
starvvx <vaibhavvstar31@gmail.com>
2021-06-14 12:17:35 +0530

    CompareTo method in implements class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        Departments se = new softwareEngineering("Software Engineering","Ruchika Malhotra",10,127);
-        Departments se1 = new softwareEngineering("Software Engineering","Ruchika Malhotra",10,127);
-        System.out.println(se.equals(se1));
-        se.showDetails();
+        studentMarks [] marksInMidsem = {
+                new studentMarks("Pushkar",10,20,30),
+                new studentMarks("Jaljala",20,30,40),
+                new studentMarks("muchhad",10,10,30),
+                new studentMarks("Anupam",40,40,30)
+        };
+        Arrays.sort(marksInMidsem);
+        for(studentMarks marks: marksInMidsem) {
+            marks.getDetails();
+        }
     }
 
 }
