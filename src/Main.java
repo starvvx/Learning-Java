c715ed03f86545703d861ee5b4908e8bf741fdf7
starvvx <vaibhavvstar31@gmail.com>
2021-06-15 17:07:52 +0530

    Streams, Collectors

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Map<Integer,String> map = new HashMap<>();
-        map.put(1,"one");map.put(2,"two");map.put(3,"three");
-
-
-        System.out.println(map);
-
-
+        List<Integer> products = new ArrayList<>();
+        products.add(1);products.add(2);products.add(3);products.add(4);
+        products.add(1);products.add(2);products.add(3);products.add(4);
+        List<Integer> product = products
+                .stream()
+                .filter(inti -> inti< 3)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(product);
     }
 
 }
