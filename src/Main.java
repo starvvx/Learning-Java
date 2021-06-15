6526cbf2431bb19124fc19bb5d4ccc7c8dc5fe62
starvvx <vaibhavvstar31@gmail.com>
2021-06-15 14:51:25 +0530

    Collections: map(keySet, valueSet, entrySet)

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Collection<Integer> v= new ArrayList<Integer>();
-        v.add(10);
-        v.add(10);
-        v.add(13);
 
-        Iterator<Integer> itr = v.iterator();
-        while (itr.hasNext()) {
-            int i = itr.next();
-            if(i > 10) {
-                itr.remove();
-            }
-        }
-        System.out.println(v);
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"one");map.put(2,"two");map.put(3,"three");
+
+        Collection<Integer > ids = map.keySet();
+        Collection<String > vals = map.values();
+
+        var entries = map.entrySet();
+        System.out.println(map);
+
+
     }
 
 }
