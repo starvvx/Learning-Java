c3d2e87b0adb9e797811f3785938c1ee9804d5c7
starvvx <vaibhavvstar31@gmail.com>
2021-06-15 15:01:31 +0530

    Methods in maps

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,7 @@ public class Main {
         Map<Integer,String> map = new HashMap<>();
         map.put(1,"one");map.put(2,"two");map.put(3,"three");
 
-        Collection<Integer > ids = map.keySet();
-        Collection<String > vals = map.values();
 
-        var entries = map.entrySet();
         System.out.println(map);
 
 
