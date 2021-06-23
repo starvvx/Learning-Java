26e4f41b6c85944283688ebc911a1356617ef0fe
starvvx <vaibhavvstar31@gmail.com>
2021-06-23 21:47:25 +0530

    Threading using callable interface

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,17 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.logging.*;
 import java.util.logging.Formatter;
 
 public class Main {
-    static class Adder implements Runnable{
-        private String infile, outfile;
-        public Adder(String infile, String outfile) {
+    static class Adder implements Callable<Integer> {
+        private String infile;
+        public Adder(String infile) {
             this.infile = infile;
-            this.outfile = outfile;
         }
-        public void doadd() throws Exception{
+        public int doadd() throws IOException{
             int total = 0;
             String line = null;
             try(BufferedReader reader = Files.newBufferedReader(Paths.get(this.infile))) {
@@ -24,25 +21,39 @@ public class Main {
                     total += Integer.parseInt(line);
                 }
             }
-            try(BufferedWriter writer = Files.newBufferedWriter(Paths.get(this.outfile))) {
-                writer.write("Total: " + total);
-            }
+            return total;
         }
-        public void run() {
-            try {
-                doadd();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        public Integer call() throws IOException {
+            return doadd();
         }
     }
     public static void main(String[] args) throws InterruptedException {
         String[] infiles = {"file1.txt","file2.txt"};
         String[] outfiles = {"file1.out.txt","file2.out.txt"};
+        Future<Integer>[] results = new Future[infiles.length];
         ExecutorService es = Executors.newFixedThreadPool(3);
         for(int i = 0;i<infiles.length;i++) {
-            Adder adder = new Adder(infiles[i],outfiles[i]);
-            es.submit(adder);
+            Adder adder = new Adder(infiles[i]);
+            results[i] = es.submit(adder);
+
+        }
+        for(int i = 0;i<results.length;i++) {
+            try {
+                int value = results[i].get();
+                System.out.println(value);
+                try(BufferedWriter writer = Files.newBufferedWriter(Paths.get(outfiles[i]))) {
+                    writer.write("total: " + value);
+                }
+
+            } catch (ExecutionException e) {
+                Throwable ex = e.getCause();
+                System.out.println(ex);
+            }
+            catch (IOException e) {
+                System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+            }
+
+
         }
         try {
             es.shutdown();
