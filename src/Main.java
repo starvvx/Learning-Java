fe42c79c1cce860683b3035a3657e50eec66194d
starvvx <vaibhavvstar31@gmail.com>
2021-06-29 11:42:38 +0530

    Working with Reflections(Field and declaredField)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -7,60 +8,29 @@ import java.util.concurrent.*;
 import java.util.logging.*;
 import java.util.logging.Formatter;
 
+class temp {
+    private int x;
+    temp(int x) {
+        this.x = x;
+    }
+}
+
+
 public class Main {
-    static class Adder implements Callable<Integer> {
-        private String infile;
-        public Adder(String infile) {
-            this.infile = infile;
-        }
-        public int doadd() throws IOException{
-            int total = 0;
-            String line = null;
-            try(BufferedReader reader = Files.newBufferedReader(Paths.get(this.infile))) {
-                while((line = reader.readLine()) != null) {
-                    total += Integer.parseInt(line);
-                }
-            }
-            return total;
-        }
-        public Integer call() throws IOException {
-            return doadd();
+    public static void main(String[] args) {
+        temp t = new temp(5);
+        Class<?> c = t.getClass();
+//        doWork(t);
+        Field declaredField[] = c.getDeclaredFields();
+        for(Field field:declaredField) {
+            System.out.println(field.getName() + " " + field.getType());
         }
     }
-    public static void main(String[] args) throws InterruptedException {
-        String[] infiles = {"file1.txt","file2.txt"};
-        String[] outfiles = {"file1.out.txt","file2.out.txt"};
-        Future<Integer>[] results = new Future[infiles.length];
-        ExecutorService es = Executors.newFixedThreadPool(3);
-        for(int i = 0;i<infiles.length;i++) {
-            Adder adder = new Adder(infiles[i]);
-            results[i] = es.submit(adder);
-
-        }
-        for(int i = 0;i<results.length;i++) {
-            try {
-                int value = results[i].get();
-                System.out.println(value);
-                try(BufferedWriter writer = Files.newBufferedWriter(Paths.get(outfiles[i]))) {
-                    writer.write("total: " + value);
-                }
-
-            } catch (ExecutionException e) {
-                Throwable ex = e.getCause();
-                System.out.println(ex);
-            }
-            catch (IOException e) {
-                System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
-            }
-
-
-        }
-        try {
-            es.shutdown();
-            es.awaitTermination(2, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
-        }
-
+    public static void doWork(Object obj) {
+        Class<?> c = obj.getClass();
+        showName(c);
+    }
+    public static void showName(Class<?> theClass) {
+        System.out.println(theClass.getSimpleName());
     }
 };
